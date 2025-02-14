13bd89f1e6b90d7c7bf71cf33dbd58af8d8bc884
RogerOliveiraRS <studio.rogeroliveira@gmail.com>
2025-02-14 17:18:10 -0300

    Revert "ajustes de texto do limite"
    This reverts commit d81435a2c61b77bdab1a3927d5493f1a1993c2fd.

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 200');
+
+
+
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
 }
 
 exibirMensagemInicial();
